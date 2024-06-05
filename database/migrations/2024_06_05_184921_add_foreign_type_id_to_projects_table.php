dfe6fed6fc8d2ce8ed18ab40d7a0d59763dbcc4e
GiorgettaR <giorgetta.roberto@gmail.com>
2024-06-05 21:36:21 +0200

    FIX projects-type migration

--- database/migrations/2024_06_05_184921_add_foreign_type_id_to_projects_table.php
+++ database/migrations/2024_06_05_184921_add_foreign_type_id_to_projects_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            $table->unsignedBigInteger('types_id')->nullable()->after('title');
-            $table->foreign('types_id')->references('id')->on('types')->onDelete('set null');
+            $table->unsignedBigInteger('type_id')->nullable()->after('title');
+            $table->foreign('type_id')->references('id')->on('types')->onDelete('set null');
         });
     }
 
@@ -23,7 +23,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            $table->dropForeign('projects_type_id_foreign');
+            $table->dropForeign('projects_typeù_id_foreign');
             $table->dropColumn('type_id');
         });
     }
